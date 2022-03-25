4dcbef66742cfa2f03ce06841b7a8975109740f7
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2022-03-25 22:25:40 +0200

    Lessons react 17

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Header from "./components/Header/Header";
 import NavbarLeft from "./components/Navbar/NavbarLeft/NavbarLeft";
 import Profile from "./components/Profile/Profile";
 import NavbarRight from "./components/Navbar/NavbarRight/NavbarRight";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
-const App = () => {
+const App = (props) => {
   return (
     <div className='app-wrapper'>
         <Header />
         <NavbarLeft />
-        <Profile />
+        <div className='app-wrapper-content'>
+        {/*<Profile />*/}
+        <Dialogs />
+        </div>
         <NavbarRight />
     </div>
   );
